edaf1ec6209d5bbaeb1673de77a2057415edb66c
Artur HRYTSENKO <ahrytsen@e2r12p14.unit.ua>
2017-11-20 16:49:34 +0200

    lol

--- fillit.h
+++ fillit.h
@@ -6,7 +6,7 @@
 /*   By: ahrytsen <ahrytsen@student.unit.ua>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/12 15:26:53 by ahrytsen          #+#    #+#             */
-/*   Updated: 2017/11/19 19:54:28 by ahrytsen         ###   ########.fr       */
+/*   Updated: 2017/11/20 14:33:09 by ahrytsen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,7 +33,7 @@ struct					s_map
 void					ft_error();
 void					ft_usage();
 int						ft_reader(int fd, t_map *matrix);
-void					ft_validate(char **tetr);
+void					ft_validate(char *tetr);
 void					ft_output(t_map *map, int size);
 
 #endif
--- input.c
+++ input.c
@@ -6,37 +6,57 @@
 /*   By: ahrytsen <ahrytsen@student.unit.ua>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/19 16:02:36 by ahrytsen          #+#    #+#             */
-/*   Updated: 2017/11/19 21:12:36 by ahrytsen         ###   ########.fr       */
+/*   Updated: 2017/11/20 15:56:51 by ahrytsen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
-
-void	ft_validate(char **tetr)
+#include <stdio.h>
+void	ft_validate(char *tetr)
 {
 	int		i;
-	int		j;
 	int		conects;
+	int		sharps;
 
 	conects = 0;
 	i = 0;
-	while (tetr[i])
+	sharps = 0;
+	while (i < 20)
 	{
-		j = 0;
-		while (tetr[i][j] && (tetr[i][j] == '.' || tetr[i][j] == '#'))
-			if (tetr[i][j] == '#')
-			{
-				conects += (i < 3 && tetr[i + 1][j] == '#') ? 1 : 0;
-				conects += (tetr[i][j + 1] == '#') ? 1 : 0;
-				conects += (i > 0 && tetr[i - 1][j] == '#') ? 1 : 0;
-				conects += (j > 0 && tetr[i][j - 1] == '#') ? 1 : 0;
-				j++;
-			}
-			else
-				(tetr[i][j] != '.') ? ft_error() : j++;
-		(j != 4) ? ft_error() : i++;
+		if (tetr[i] == '#' && (i + 1) % 5 && sharps++ < 4)
+		{
+			conects += (i > 0 && tetr[i - 1] == '#') ? 1 : 0;
+			conects += (tetr[i + 1] == '#') ? 1 : 0;
+			conects += (i > 4 && tetr[i - 5] == '#') ? 1 : 0;
+			conects += (i < 14 && tetr[i + 5] == '#') ? 1 : 0;
+		}
+		else if (((i + 1) % 5 && tetr[i] != '.')
+				 || (!(i + 1) % 5 && tetr[i] != '\n'))
+			ft_error();
+		i++;
 	}
-	(i != 4 || (conects != 6 && conects != 8)) ? ft_error() : 0;
+	if ((tetr[i] && tetr[i] != '\n') || (conects != 6 && conects != 8))
+		ft_error();
+}
+
+t_64bit	ft_getvalue(char **tetr)
+{
+	t_64bit	value;
+	int i;
+
+	value = 0;
+	if (!tetr)
+		ft_error();
+	i = 0;
+	while(i < 16)
+	{
+		if (i && tetr[i / 4][i % 4] == '#')
+			value |= 1L << (63 - 16 * (i / 4) - (i % 4));
+		else if (!i && tetr[0][0] == '#')
+			value |= 1L << 63;
+		i++;
+	}
+	return (value);
 }
 
 /*
@@ -47,26 +67,24 @@ void	ft_validate(char **tetr)
 
 int		ft_reader(int fd, t_map *matrix)
 {
-	char	**tetrimino;
 	char	tmp[22];
 	t_64bit	value;
-	int		count;
+	int		count[2];
 	int		i;
 
 	i = 0;
 	ft_bzero(tmp, 22);
 	matrix = NULL;
 	value = 0;
-	while ((count = read(fd, tmp, 21)))
+	while ((count[0] = read(fd, tmp, 21)))
 	{
-		if (count < 20 || i++ > 25
-			|| !(tetrimino = ft_strsplit(tmp, '\n')))
-			ft_error();
-		ft_validate(tetrimino);
-		free(tetrimino);
+		(i++ > 25 || count[0] < 20) ? ft_error() : ft_validate(tmp);
+		value = ft_getvalue(ft_strsplit(tmp, '\n'));
 		ft_bzero(tmp, 22);
+		count[1] = count[0];
+		printf("%lu\n", value);
 	}
-	if (close(fd))
+	if (close(fd) || !i || count[1] != 20)
 		ft_error();
-	return (0);
+	return (i);
 }
